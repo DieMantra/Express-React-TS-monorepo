d55dc5aea4c9958a5ec9f13cac49b8309b060444
Tyson <100465135+DieMantra@users.noreply.github.com>
2023-04-07 14:29:29 +1000

    test

--- @apps/server/rollup.config.js
+++ @apps/server/rollup.config.js
@@ -6,7 +6,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
 	input: './src/index.ts',
 	output: {
-		file: './dist/index.js',
+		file: './api/index.js',
 		format: 'cjs',
 		plugins: [terser()],
 	},
